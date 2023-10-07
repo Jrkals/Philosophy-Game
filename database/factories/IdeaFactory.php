c6cb21e46db78f8f24e6626150f18a685ff5625e
Justin Kalan <jkalan@wordonfire.org>
2023-10-07 12:33:14 -0500

    add idea seeder

---
+++ database/factories/IdeaFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Idea;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class IdeaFactory extends Factory {
+    protected $model = Idea::class;
+
+    public function definition(): array {
+        return [
+            'points' => 0,
+            'winner' => false,
+        ];
+    }
+}
---
+++ database/seeders/IdeaSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Game;
+use App\Models\Idea;
+
+class IdeaSeeder extends \Illuminate\Database\Seeder {
+
+    public function run( Game $game ): void {
+        $atheism             = Idea::factory( [
+            'name'     => 'Atheism',
+            'category' => 'Theism'
+        ] )->for( $game )->create();
+        $classicalTheism     = Idea::factory( [
+            'name'     => 'Classical Theism',
+            'category' => 'Theism'
+        ] )->for( $game )->create();
+        $theisticPersonalism = Idea::factory( [
+            'name'     => 'Theistic Personalism',
+            'category' => 'Theism'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Pantheism',
+            'category' => 'Theism'
+        ] )->for( $game )->create();
+    }
+}
