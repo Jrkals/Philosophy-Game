11f7bde4ba04171ca940580ec61e3039f81a2434
Justin Kalan <jkalan@wordonfire.org>
2023-10-09 19:03:52 -0500

    added ideas

--- database/seeders/IdeaSeeder.php
+++ database/seeders/IdeaSeeder.php
@@ -27,5 +27,73 @@ class IdeaSeeder extends \Illuminate\Database\Seeder {
             'name'     => 'Pantheism',
             'category' => 'Theism'
         ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Monarchy',
+            'category' => 'Politics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Democracy',
+            'category' => 'Politics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Aristocracy',
+            'category' => 'Politics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Jesus is God',
+            'category' => 'Jesus'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Jesus is mere man',
+            'category' => 'Jesus'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Hylomorphism',
+            'category' => 'Metaphysics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Platonism',
+            'category' => 'Metaphysics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Nominalism',
+            'category' => 'Metaphysics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Virtue Ethics',
+            'category' => 'Ethics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Deontology',
+            'category' => 'Ethics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Utilitarianism',
+            'category' => 'Ethics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Nihilism',
+            'category' => 'Ethics'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Thomism',
+            'category' => 'Method'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Analytic',
+            'category' => 'Method'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Continental',
+            'category' => 'Method'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Rationalism',
+            'category' => 'Epistemology'
+        ] )->for( $game )->create();
+        $pantheism           = Idea::factory( [
+            'name'     => 'Empiricism',
+            'category' => 'Epistemology'
+        ] )->for( $game )->create();
     }
 }
